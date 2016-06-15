ae28aeec4fb1407e5f6b1c59a0f660be6166a890
Mauricio van der Maesen <mauricio.van.der.maesen@iqu.com>
2016-06-15 10:36:41 +0200

    add getter and setter for dim2 in invoiceLines

--- src/Pronamic/Twinfield/Invoice/InvoiceLine.php
+++ src/Pronamic/Twinfield/Invoice/InvoiceLine.php
@@ -230,5 +230,16 @@ class InvoiceLine
         return $this;
     }
 
+    public function getDim2()
+    {
+        return $this->dim2;
+    }
+
+    public function setDim2($dim2)
+    {
+        $this->dim2 = $dim2;
+        return $this;
+    }
+
 
 }
